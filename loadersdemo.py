eb2f7e6a976b6b86a32f1c6ee3d2eb53253b373a
Mallikha Kaparaveni <mallikha.kaparaveni@accellor.com>
2025-10-14 21:55:02 +0530

    initial

---
+++ loadersdemo.py
@@ -0,0 +1,21 @@
+from langchain_community.document_loaders import PyPDFLoader,TextLoader,CSVLoader, WebBaseLoader
+
+"""document=PyPDFLoader('comp-off.pdf')
+data=document.load()
+print(data[1])
+
+document1=PyPDFLoader("C:\\Users\\MallikhaKaparaveni\\Downloads\\water.pdf")
+data1=document1.load()
+print(data1[0])
+
+document2=TextLoader("C:\\Users\\MallikhaKaparaveni\\OneDrive - Accellor\\Documents\\langchainfile.txt")
+data2=document2.load()
+print(data2[0])
+
+document3=CSVLoader("C:\\Users\\MallikhaKaparaveni\\Downloads\\day.csv")
+data3=document3.load()
+print(data3[5])"""
+
+document4=WebBaseLoader("https://en.wikipedia.org/wiki/Artificial_intelligence")
+data4=document4.load()
+print(data4[0].page_content[:700])
