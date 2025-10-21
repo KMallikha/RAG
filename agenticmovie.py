ff4b38a2e47ae4bf0fa3c2749864a9a1086cc116
Mallikha Kaparaveni <mallikha.kaparaveni@accellor.com>
2025-10-21 16:50:24 +0530

    adding the code even though few answers wrong in moviesrag when tested and agent is not working properly

--- agenticmovie.py
+++ agenticmovie.py
@@ -4,7 +4,8 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.agents import initialize_agent,AgentType
+from langchain.tools import Tool
 from dotenv import load_dotenv
 import os
 
@@ -19,32 +20,44 @@ docdata=docs.load()
 rp=r.split_documents(docdata)"""
 rp=docdata
 
-emb=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-                          #all-MiniLM-L6-v2")
-
+emb=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 #vec_emb=Chroma.from_documents(documents=rp,embedding=emb,collection_name="moviedata",persist_directory="./chromadb")
 db = Chroma(persist_directory="./chromadb", embedding_function=emb,collection_name="moviedata")
 """re=db.similarity_search("what is ocean pollution",k=2)
 print(re)"""
 
-llm=ChatGroq(api_key=apikey,model="llama-3.3-70b-versatile")
-retriever= db.as_retriever(search_kwargs={"k":len(rp)})
+llm=ChatGroq(api_key=apikey,model="groq/compound",temperature=0)
+retriever= db.as_retriever(search_kwargs={"k":len(rp),"score_threshold":0.7})
 template="""you are a helpful assistant 
 -if user greets reply with greetings.
--if user asks question and if and only if strictly related to provided context, answer the question using the Documents.
+-if user asks question and if and only if strictly related to provided context, only answer the question based on given documents using the tools.
 -if it is not related to the context strictly tell information is not related,i don't know
 Do not guess or describe anything not present in the given document.
-never use external knowledge or prior memory or internet"""
+never use external knowledge or prior memory or internet
 
-prompt=ChatPromptTemplate.from_messages([("system",template),("human","question:{question} documents:{context}")])
+question:{question}
+documents:{context}
+"""
+
+prompt=ChatPromptTemplate.from_template(template)
 chain=RetrievalQA.from_chain_type(llm=llm,retriever=retriever,chain_type="stuff",chain_type_kwargs={"prompt":prompt},return_source_documents=True)
 
+tools=[Tool(
+    name="movierag",
+    func=chain.run,
+    description="""Use this tool to answer movie-related questions.
+You MUST ONLY use the CSV data returned by the tool.
+Do not answer any questions using your own knowledge.
+If no answer is found in the CSV, respond with 'No data available."""
+)]
+
+agent=initialize_agent(tools=tools,llm=llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True,handle_parsing_errors=True)
 
 while True:
     ip=input("Query: ")
     if ip=="Bye" or ip=="bye":
         print("goodbye")
         break
-    result=chain.invoke({"query":ip})
-    print(result["result"])
+    result=agent.run(ip)
+    print(result)
